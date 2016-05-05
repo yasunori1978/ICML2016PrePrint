43dd68596bd1addfdda102ed90cfb30173946cd1
石井育規 <ishii@phi.local>
2016-05-05 09:41:12 +0900

    downloader commit third

--- cvpr2016downloader_deep.py
+++ cvpr2016downloader_deep.py
@@ -21,6 +21,32 @@ for l in [ line  for line in res.readlines() if '<strong>' in line ]:
 
             if '<title>' in l:
                 l_title = l.split('<title>')[1].split('</title>')[0].rstrip().replace('&#8220','“').replace('&#8221','”').replace(':',' ').replace('<small>','').replace('</small>','').replace('<i>','').replace('</i>','').replace('<sup>','').replace('</sup>','').replace('-',' ').replace('&#8212',"—").replace('&#8216',"‘").replace('&#8217',"’").replace('&#239','ï').replace('&#246','ö').replace('&#126','~').replace('&#8211','–')  
+                if 'deep' in l:
+                    deep = 1
+                elif 'Eeep' in l:
+                    deep = 1
+                elif 'neural' in l:
+                    deep = 1
+                elif 'Neural' in l:
+                    deep = 1
+                elif 'convolutional' in l:
+                    deep = 1
+                elif 'Convolutional' in l:
+                    deep = 1
+                elif 'recurrent' in l:
+                    deep = 1
+                elif 'Recurrent' in l:
+                    deep = 1
+                elif 'LSTM' in l:
+                    deep = 1
+                elif 'Long Short' in l:
+                    deep = 1
+                elif 'network' in l:
+                    deep = 1
+                elif 'Network' in l:
+                    deep = 1
+
+
                 
             if '</arxiv:comment>' in l:
                 if 'CVPR 2016' in l:
@@ -31,7 +57,7 @@ for l in [ line  for line in res.readlines() if '<strong>' in line ]:
                 pdf = l.split('"')[3]
             if '<name>' in l:
                 authors.append(l)
-            if '<summary>' in l or sum == 1:
+            if '<summary>' in l or sum == 1  :
                 sum = 1
                 if 'deep' in l:
                     deep = 1
